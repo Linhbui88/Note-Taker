d2993d2c0d530983422fb964431b6806769350ab
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-11-01 17:10:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const apiRoutes = require('./routes')
+
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
+
+
+app.use('/api',apiRoutes)/
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
